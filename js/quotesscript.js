932e38c5b5a2539f2f78085bba8f1cf5a9b7560d
Olivia Perryman <Olivia@perryman-mac.local>
2017-11-07 22:15:01 -0500

    server

---
+++ js/quotesscript.js
@@ -0,0 +1,21 @@
+
+
+
+$(document).ready(function(){
+  $.get("titles.html", function(data){
+      $("#quotes-list").append(data);
+      var quotes = $(".quotes");
+      var quoteIndex = -1;
+
+          function showNextQuote() {
+              ++quoteIndex;
+              quotes.eq(quoteIndex % quotes.length)
+                  .fadeIn(2000)
+                  .delay(2000)
+                  .fadeOut(2000, showNextQuote);
+          }
+
+          showNextQuote();
+    });
+
+});
